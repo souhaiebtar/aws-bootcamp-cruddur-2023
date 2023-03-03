db7182c8407e6919eac263a20da2c58cd9a9b8ba
souhaiebtar <tarhounisouhaieb@gmail.com>
2023-03-03 22:20:31 +0000

    fix typo

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,7 +4,7 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run("""logger):
+  def run():
     # logger.info("HomeActivities")
     tracer = trace.get_tracer(__name__)
     with tracer.start_as_current_span("home-activities-mock-data"):
